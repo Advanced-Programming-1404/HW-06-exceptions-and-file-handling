41f488a81a44a153e55c4a8ed5856c45a9d96ba7
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2026-05-14 16:20:00 +0330

    feat: update Main.java todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     static ArrayList<Product> productCatalog = new ArrayList<>();
-    public static void loadProducts() throws IOException {
+    public static void loadProducts() throws IOException{
         InputStream input = Main.class
                 .getClassLoader()
                 .getResourceAsStream("products.csv");
+        if (input == null) {
+            throw new IOException("products.csv not found");
+        }
         Scanner scanner = new Scanner(input);
 
         // TODO:
@@ -27,12 +31,16 @@ public class Main {
         try {
             loadProducts();
         } catch (IOException e) {
-            System.err.println("Something went wrong with loading products!\nterminating the program...");
+            System.err.println("Error reading products");
+            System.out.println("Terminating the program...");
             return;
         }
+        System.out.println("=== Products loaded");
 
-        ReportGenerator reportGenerator = new ReportGenerator("2025_order_details.csv", productCatalog);
+        ReportGenerator reportGenerator = new ReportGenerator("2025_order_details.csv", Collections.unmodifiableList(productCatalog));
         reportGenerator.processFile();
+        System.out.println("=== Order file processed");
         reportGenerator.saveReport();
+        System.out.println("=== Report saved as report.txt");
     }
 }
